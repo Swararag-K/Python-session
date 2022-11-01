3524d867b246199ababbdfb58bd95220b7c7cd96
Swararag-K <94892211+Swararag-K@users.noreply.github.com>
2022-11-01 17:29:04 +0530

    Update pizza_toppings.py

--- day3/pizza_toppings.py
+++ day3/pizza_toppings.py
@@ -1,9 +1,10 @@
 toppings=[]
 print("Enter the toppings")
 for i in range(100):
-    toppings.append(input())
-    if toppings[i]=="quit":
+    X=input()
+    if X=="quit":
         break
+    toppings.append(X)
     
 print("Your toppings are")
 print(toppings)
