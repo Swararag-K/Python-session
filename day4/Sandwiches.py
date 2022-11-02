485412fac30d1109caa78792010b77ceffce9573
Swararag-K <94892211+Swararag-K@users.noreply.github.com>
2022-11-02 16:52:59 +0530

    Create Sandwiches.py

---
+++ day4/Sandwiches.py
@@ -0,0 +1,7 @@
+sandwich_orders=["Chicken","veg","Cheese","Tuna"]
+finished_sandwiches=[]
+for i in sandwich_orders:
+    print("I made your ",i," sandwich")
+    finished_sandwiches.append(i)
+    
+print(finished_sandwiches) 
